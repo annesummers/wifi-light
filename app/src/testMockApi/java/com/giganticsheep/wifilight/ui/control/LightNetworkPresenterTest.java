c96b64c6f477cdecf505dafa547f39aac36a9646
Anne Summers <annesummers@onebadmouse.com>
2015-07-26 18:35:26 +0100

    Added the brightness label back in which hangs the ui tests

---
+++ app/src/testMockApi/java/com/giganticsheep/wifilight/ui/control/LightNetworkPresenterTest.java
@@ -0,0 +1,41 @@
+package com.giganticsheep.wifilight.ui.control;
+
+import org.junit.Test;
+
+/**
+ * DESCRIPTION HERE ANNE <p>
+ * Created by anne on 26/07/15. <p>
+ * (*_*)
+ */
+public class LightNetworkPresenterTest {
+
+    @Test
+    public void testFetchLight() throws Exception {
+
+    }
+
+    @Test
+    public void testOnEvent() throws Exception {
+
+    }
+
+    @Test
+    public void testOnEvent1() throws Exception {
+
+    }
+
+    @Test
+    public void testOnEvent2() throws Exception {
+
+    }
+
+    @Test
+    public void testOnEvent3() throws Exception {
+
+    }
+
+    @Test
+    public void testOnEvent4() throws Exception {
+
+    }
+}
